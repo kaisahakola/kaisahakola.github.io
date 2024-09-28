af6e9dfa05a5a0d431bd0a1f514e2e126876c1d7
Kaisa Hakola <kaisahakola@Kaisa-MacBook-Pro.local>
2024-09-28 14:33:17 +0300

    First version of navbar

---
+++ src/components/Navbar/Navigation.tsx
@@ -0,0 +1,38 @@
+import { NavLink, Link } from "react-router-dom";
+import Navbar from "react-bootstrap/Navbar";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import "./Navbar.scss"
+
+const Navigation = () => {
+  return (
+    <div className="navbar">
+      <Navbar expand="lg">
+        <Container>
+          <Navbar.Brand as={Link} to="/" className="nav-link-brand">
+            &lt;insert brand here&gt;
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto">
+              <Nav.Link as={NavLink} to="/"  className="nav-link">
+                Home
+              </Nav.Link>
+              <Nav.Link as={NavLink} to="/about-me" className="nav-link">
+                About me
+              </Nav.Link>
+              <Nav.Link as={NavLink} to="/projects" className="nav-link">
+                Projects
+              </Nav.Link>
+              <Nav.Link as={NavLink} to="/contacts" className="nav-link">
+                Contacts
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </div>
+  );
+};
+
+export default Navigation;
