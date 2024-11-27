5e9bba9eb6d4989d0fcb8de79057d86b7e6512e4
Kaisa Hakola <kaisahakola00@gmail.com>
2024-11-27 15:45:08 +0200

    Add content to contacts page

---
+++ src/shared/const/ContactInfo.ts
@@ -0,0 +1,6 @@
+export const ContactInfo = {
+    email: "kaisahakola00@gmail.com",
+    phone: "0400193396",
+    github: "github.com/kaisahakola",
+    linkedIn: "www.linkedin.com/in/khakola/"
+} as const
