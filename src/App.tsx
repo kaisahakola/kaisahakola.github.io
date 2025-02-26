0a2d3f9c5a1a9d7ad599492b91734c30fa81100c
Kaisa Hakola <kaisahakola00@gmail.com>
2025-02-26 15:03:43 +0200

    some important things
    - new home page design
    - new scss variables
    - face image

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import "./App.css";
-import Header from "./components/header/Header";
+import "./App.scss";
+import MainPage from "./pages/mainPage/MainPage.tsx";
 import ProjectList from "./components/projects/ProjectList";
 import Contacts from "./components/Contacts/Contacts";
 import AboutMe from "./components/aboutMe/AboutMe";
@@ -7,17 +7,12 @@ import Navigation from "./components/Navbar/Navigation";
 import { Route, Routes } from "react-router-dom";
 
 function App() {
-  const heading = {
-    header: "kaisa hakola",
-    subtitle: "Software Developer",
-  };
-
   return (
     <div>
       <Navigation />
 
       <Routes>
-        <Route path="/" element={<Header heading={heading} />} />
+        <Route path="/" element={<MainPage />} />
         <Route path="/about-me" element={<AboutMe />} />
         <Route path="/projects" element={<ProjectList />} />
         <Route path="/contacts" element={<Contacts />} />
---
+++ src/shared/const/Heading.ts
@@ -0,0 +1,5 @@
+export const Heading = {
+    header: "Kaisa Hakola",
+    subtitle: "Software Developer",
+    description: "Hello, my name is"
+} as const
