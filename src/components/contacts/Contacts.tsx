91309d5fb0fc2f456b471c9e32354fe9df160fce
Kaisa Hakola <kaisahakola00@gmail.com>
2025-02-27 18:37:16 +0200

    take phone info off

--- src/components/contacts/Contacts.tsx
+++ src/components/contacts/Contacts.tsx
@@ -1,22 +1,17 @@
 import { ContactInfo } from "../../shared/const/ContactInfo";
 import Linkdin from "../../assets/icons/linkedIn2.png";
 import Github from "../../assets/icons/github.webp";
-import Phone from "../../assets/icons/phoneIcon.webp";
 import Email from "../../assets/icons/emailIcon.webp";
 import cls from "./Contacts.module.scss";
 
 const Contacts = () => {
-  const { email, phone, github, linkedIn } = ContactInfo;
+  const { email, github, linkedIn } = ContactInfo;
   return (
     <div className={cls.Wrapper}>
       <h1>Contact info</h1>
       <div className={cls.Container}>
         <img className={cls.Icon} src={Email} alt="Email icon" />
         <p className={cls.Text}>{email}</p>
-      </div>
-      <div className={cls.Container}>
-        <img className={cls.Icon} src={Phone} alt="Phone icon" />
-        <p className={cls.Text}>{phone}</p>
       </div>
       <div className={cls.Container}>
         <img className={cls.Icon} src={Github} alt="Github logo" />
