3358edec09d9f184d8f22172e7ea30a27f10a813
Kaisa Hakola <kaisahakola00@gmail.com>
2025-02-27 18:36:11 +0200

    Update ContactInfo.ts

--- src/shared/const/ContactInfo.ts
+++ src/shared/const/ContactInfo.ts
@@ -1,6 +1,5 @@
 export const ContactInfo = {
     email: "hakola.kaisa@icloud.com",
-    phone: "0400193396",
     github: "github.com/kaisahakola",
     linkedIn: "www.linkedin.com/in/khakola/"
 } as const
